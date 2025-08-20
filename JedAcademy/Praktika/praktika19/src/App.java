8014e20e36165f274ebd349da593f1637d1c1195
WebDestiny90 <AzeAgent@gmail.com>
2025-08-20 11:54:44 +0400

    20.08.25 Practice

--- JedAcademy/Praktika/praktika19/src/App.java
+++ JedAcademy/Praktika/praktika19/src/App.java
@@ -1,5 +1,46 @@
-public class App {
-  public static void main(String[] args) throws Exception {
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class App {
+  static void check(int age) throws MyException {
+    if (age < 18) {
+      throw new MyException("Yaş 18-dən kiçik ola bilməz!");
+    }
+  }
+
+  public static void main(String[] args) {
+
+    try {
+      check(15);
+    } catch (MyException e) {
+      System.out.println(e.getMessage());
+    }
+
+    // try {
+    // int a = 10;
+    // int b = 0;
+    // int result = a / b;
+    // } catch (ArithmeticException e) {
+    // System.out.println("Sıfıra bölmək olmaz!");
+    // }
+
+    // Scanner sc = new Scanner(System.in);
+    // int age = 0;
+
+    // while (age != 18) {
+    // try {
+    // System.out.print("Yaşınızı daxil edin: ");
+    // age = sc.nextInt();
+
+    // if (age != 18) {
+    // System.out.println("Düzgün yaş deyil, yenidən cəhd edin.");
+    // }
+
+    // } catch (InputMismatchException e) {
+    // System.out.println("Xəta: Rəqəm daxil edin!");
+    // sc.nextLine();
+    // }
+    // }
+    // System.out.println("Düzgün yaş daxil etdiniz: " + age);
   }
 }
